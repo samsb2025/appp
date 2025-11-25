2dadfb9ab0cc520ffc06079d97034a41cb759793
samsb2025 <samtil2000b@gmail.com>
2025-11-25 15:59:02 +0300

    25

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import routes from "./routes.js";
+import routes from "./src/routes.js";
 
 const app = express();
 app.use(cors());
